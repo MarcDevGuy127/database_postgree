29a9d6373df37e3ff65fd3bead066e2e70d3494e
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-29 17:41:29 -0300

    feat: add exercise #2 resolution

--- views/views_exercises.sql
+++ views/views_exercises.sql
@@ -1,3 +1,5 @@
+
+
 --Exercicio 1
 --Crie uma view para retornar o id do usuário, o seu 
 --nome, cpf (no caso de hospede) ou cnpj (no caso de 
@@ -6,10 +8,28 @@
 SELECT * FROM usuario;
 
 CREATE VIEW view_Users
-AS SELECT COUNT(id_usuario, nome, email
+AS SELECT COUNT(id_usuario, nome, email)
 FROM usuario
 WHERE = (SELECT cpf FROM hospede);
 
 --DROP VIEW view_Users;
 
 SELECT * FROM view_Users;
+
+--Exercicio 2
+--Crie uma view para retornar o id do imovel, o título do 
+--imóvel, o valor da diária e o nome do tipo do imóvel.
+
+SELECT * FROM tipo_imovel;
+SELECT * FROM imovel;
+
+drop view view_Houses;
+
+CREATE VIEW view_Houses AS
+SELECT id_imovel, titulo, tipo_imovel.rotulo, AVG(valor_diaria) 
+FROM imovel
+INNER JOIN tipo_imovel
+ON imovel.tipo_codigo = tipo_imovel.codigo
+GROUP BY imovel.id_imovel, imovel.titulo, tipo_imovel.rotulo;
+
+SELECT * FROM view_Houses;
