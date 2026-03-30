bcb218ba506397319ecada9a3ee252c28e163809
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-30 10:37:56 -0300

    feat: add view_Servicos for exercise #2

--- views/mecanicoapp_views.sql
+++ views/mecanicoapp_views.sql
@@ -5,7 +5,28 @@ FROM mecanico
 ORDER BY especialidade ASC;
 
 SELECT * FROM view_Mecanicos;
+
 --2. Mostrar os serviços realizados na loja, com o nome do usuario, do mecânico e da peça, e o preço do serviço
+DROP VIEW view_Servicos;
+CREATE VIEW view_Servicos AS
+SELECT usuario.nome AS username,
+mecanico.nome AS mechanic,
+servico.descricao AS service,
+peca.nome AS piece,
+servico.valor_servico AS service_value,
+loja.nome AS auto_repair
+FROM mecanico
+INNER JOIN servico
+ON mecanico.id_mecanico = servico.id_usuario
+INNER JOIN usuario
+ON servico.id_usuario = usuario.id_usuario
+INNER JOIN peca
+ON usuario.id_usuario = peca.id_peca
+INNER JOIN loja
+ON peca.id_peca = loja.id_loja
+ORDER BY usuario.nome ASC;
+
+SELECT * FROM view_Servicos;
 --3. Mostrar o nome das peças e a quantidade em estoque delas
 --4. Mostrar o nome dos lojistas e da loja na qual ele trabalha
 --5. Mostrar a receita bruta faturada na loja de peças
