cc6b0e9879ee059c9eb24ffd7b6297d13ea026e1
MarcDevGuy127 <marceloh.github@gmail.com>
2026-05-04 21:27:47 -0300

    pousadapp: test raise exception

--- pousadapp/plpsql_trigger_class.sql
+++ pousadapp/plpsql_trigger_class.sql
@@ -31,3 +31,47 @@ INSERT INTO usuario (nome, email, telefone)
 VALUES ('Maria Souza', 'MARIA.SOUZA@EMAIL.COM', '41988887777');
 
 SELECT * FROM usuario ORDER BY id_usuario DESC;
+
+select * from reserva;
+--PRACTICING
+CREATE OR REPLACE FUNCTION valor_total_fnc()
+RETURNS TRIGGER AS $$
+DECLARE
+	qntde_hospedes INTEGER;
+	qntde_dias INTEGER;
+	valor_total NUMERIC(10,2);
+BEGIN
+	qntde_hospedes := NEW.qtd_hospedes;
+	qntde_dias := NEW.checkout - NEW.checkin;
+
+	IF qntde_hospede = 1 THEN
+  	valor_total = NEW.valor_total;
+	ELSEIF qntde_hospede >= 2 THEN
+  	valor_total = NEW.valor_total * 0.50;
+	END IF; 
+	
+	NEW.valor_total := valor_total;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--RAISE EXCEPTION
+CREATE OR REPLACE FUNCTION valida_diaria_fnc()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.valor_diaria < 0 THEN
+RAISE EXCEPTION 'O preço da diaria não pode 
+ser negativo (preço %)', NEW.valor_diaria;
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS validar_diaria_trg ON 
+imovel;
+CREATE TRIGGER trg_validar_diaria
+BEFORE INSERT OR UPDATE ON imovel
+FOR EACH ROW
+EXECUTE FUNCTION valida_diaria_fnc();
+
+UPDATE imovel SET valor_diaria = -50 WHERE id_imovel = 1;
