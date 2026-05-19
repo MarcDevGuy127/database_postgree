7f28f2b132aecd9ed855485ac30aec58fef2cfc2
MarcDevGuy127 <marceloh.github@gmail.com>
2026-05-18 21:05:11 -0300

    test: add resumed math parameter

--- pousadapp/transactions.sql
+++ pousadapp/transactions.sql
@@ -8,7 +8,7 @@ SUM(valor_diaria) AS soma
 FROM imovel;
 
 UPDATE imovel
-SET valor_diaria = valor_diaria + (valor_diaria * 0.10)
+SET valor_diaria = valor_diaria * 1.1
 WHERE tipo_codigo IN (1,2);
 
 SELECT AVG(valor_diaria) AS media,
